4a87fa989d227b3055107e4aec88b1d2bfe1b990
Michał Bąk <michalbak000@gmail.com>
2026-03-18 16:01:16 +0100

    Lab1 Zad4

---
+++ Lab1_2/Zad4/src/Pozdrowienia.java
@@ -0,0 +1,13 @@
+public class Pozdrowienia {
+    public void pozdrowienia1() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Pozdrowienia z wątku::pozdrowienia1::" + i);
+        }
+    }
+
+    public static void pozdrowienia2() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Pozdrowienia z wątku::pozdrowienia2::" + i);
+        }
+    }
+}
---
+++ Lab1_2/Zad4/src/Test.java
@@ -0,0 +1,25 @@
+public class Test {
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Pozdrowienia z wątku:: lambda" + i);
+            }
+        });
+        t1.start();
+
+        Thread t2 = new Thread(new Pozdrowienia()::pozdrowienia1);
+        t2.start();
+
+        Thread t3 = new Thread(Pozdrowienia::pozdrowienia2);
+        t3.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException e) {}
+
+        System.out.println("Koniec");
+    }
+}
