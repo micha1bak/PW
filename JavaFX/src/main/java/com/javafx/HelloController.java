0a86b8da15f82960283e8dd4ca758abfc3a2044b
Michał Bąk <michalbak000@gmail.com>
2026-05-20 13:05:58 +0200

    color picker and circle

--- JavaFX/src/main/java/com/javafx/HelloController.java
+++ JavaFX/src/main/java/com/javafx/HelloController.java
@@ -1,14 +1,28 @@
 package com.javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private ColorPicker colorPicker;
+
+    @FXML
+    private Circle circle;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void onColortPicker(ActionEvent actionEvent) {
+        circle.setFill(colorPicker.getValue());
     }
 }
