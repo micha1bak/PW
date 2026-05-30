8c68a8b26db70a02837d8489dcb8c36288b5139d
Michał Bąk <michalbak000@gmail.com>
2026-05-30 13:56:14 +0200

    javafx demo

--- JavaFX/src/main/java/com/javafx/HelloApplication.java
+++ JavaFX/src/main/java/com/javafx/HelloApplication.java
@@ -2,7 +2,9 @@ package com.javafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Group root = new Group();
+        Scene scene = new Scene(root, 1000, 800);
+        Button button = new Button("Hello World");
+        root.getChildren().add(button);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
