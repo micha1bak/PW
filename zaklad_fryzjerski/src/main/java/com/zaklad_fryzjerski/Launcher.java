ebc10aff3e72cfb323d0dd907b9a02528e16ea39
Michał Bąk <michalbak000@gmail.com>
2026-05-30 14:12:04 +0200

    zaklad fryzjerski final project init

---
+++ zaklad_fryzjerski/src/main/java/com/zaklad_fryzjerski/Launcher.java
@@ -0,0 +1,9 @@
+package com.zaklad_fryzjerski;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
