c7471cfe91c4d3727fecec955a3126e51468aae1
Michał Bąk <michalbak000@gmail.com>
2026-06-15 15:21:54 +0200

    refactor

--- zaklad_fryzjerski/src/main/java/com/zaklad_fryzjerski/model/SimulationManager.java
+++ zaklad_fryzjerski/src/main/java/com/zaklad_fryzjerski/model/SimulationManager.java
@@ -9,22 +9,21 @@ public class SimulationManager {
     private final List<Barber> barbers = new ArrayList<>();
     private final ClientGenerator generator;
 
-    private SimulationObserver observer;
-
     public SimulationManager(int n, int pTotal, int l, int capacity, List<Set<Integer>> specializations) {
         this.shop = new BarberShop(capacity, l);
+        initializeBarbers(pTotal, specializations);
+        this.generator = new ClientGenerator(shop, n);
+    }
 
+    private void initializeBarbers(int pTotal, List<Set<Integer>> specializations) {
         for (int i = 0; i < pTotal; i++) {
             Set<Integer> specs = (i < specializations.size()) ? specializations.get(i) : Set.of(1);
             Barber barber = new Barber(i + 1, specs, shop);
             barbers.add(barber);
         }
-        
-        this.generator = new ClientGenerator(shop, n);
     }
 
     public void setObserver(SimulationObserver observer) {
-        this.observer = observer;
         this.shop.setObserver(observer);
         for (Barber b : barbers) b.setObserver(observer);
     }
@@ -38,7 +37,4 @@ public class SimulationManager {
         generator.stopGenerator();
         for (Barber b : barbers) b.stopBarber();
     }
-
-    public BarberShop getShop() { return shop; }
-    public List<Barber> getBarbers() { return barbers; }
 }
